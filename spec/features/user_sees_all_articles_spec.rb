06e26b16ab63d71bb7c873579f35ed6439c9af8b
agpiermarini <piermarini.andrew@gmail.com>
2018-03-22 20:02:44 -0600

    Add links to each article. Add create new article link.

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -2,20 +2,20 @@ require "rails_helper"
 
 
 describe "user sees all articles" do
-
-  before(:each) do
-    article_1 = Article.create!(title: "Title 1", body: "Body 1")
-    article_2 = Article.create!(title: "Title 2", body: "Body 2")
-  end
-
   describe "they visit /articles" do
-    it "displays all artciles" do
+    it "displays all articles" do
+      article_1 = Article.create!(title: "Title 1", body: "Body 1")
+      article_2 = Article.create!(title: "Title 2", body: "Body 2")
       visit '/articles'
 
       expect(page).to have_content(article_1.title)
+      expect(page).to have_link(article_1.title)
       expect(page).to have_content(article_1.body)
       expect(page).to have_content(article_2.title)
+      expect(page).to have_link(article_2.title)
       expect(page).to have_content(article_2.body)
+
+      expect(page).to have_link("Create a New Article")
     end
   end
 end
