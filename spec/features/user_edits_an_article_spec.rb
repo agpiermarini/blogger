d3a777d11e143c96ee54643754781cb522dc6710
agpiermarini <piermarini.andrew@gmail.com>
2018-03-22 23:32:09 -0600

    Re-adding create article functionality

---
+++ spec/features/user_edits_an_article_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+
+describe "user edits an article" do
+  describe "they link from the show page" do
+    it "displays all articles with edited information" do
+      article_1 = Article.create!(title: "Title 1", body: "Body 1")
+      article_2 = Article.create!(title: "Title 2", body: "Body 2")
+
+      visit article_path(article_1)
+      click_link "Edit"
+
+      fill_in "article[title]", with: "New Article Title"
+      fill_in "article[body]", with: "New Article Body"
+      click_on "Update Article"
+
+      expect(current_path).to eq(articles_path)
+      expect(page).to have_content("New Article Title")
+      expect(page).to have_content("New Article Body")
+    end
+  end
+end
