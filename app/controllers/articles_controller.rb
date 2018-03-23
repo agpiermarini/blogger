d14e2517d4eab0f4031b745bc3d36dcd8f368149
agpiermarini <piermarini.andrew@gmail.com>
2018-03-22 22:19:56 -0600

    Adds new article functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,9 +6,24 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    require "pry"; binding.pry
     render locals: {
               article:  Article.find(params[:id])
               }
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
---
+++ spec/features/user_creates_new_article_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+
+describe "user creates a new article" do
+  describe "they link from the articles index" do
+    it "they fill in the title and body" do
+      visit articles_path
+      click_link("Create a New Article")
+
+      fill_in "article[title]", with: "Did I Mention Dogs Were Great?"
+      fill_in "article[body]", with: "No? Well, let me tell you."
+      click_on "Create Article"
+
+      # save_and_open_page #this isnt causing anything to appear in server tab
+
+      expect(page).to have_content("Did I Mention Dogs Were Great?")
+      expect(page).to have_content("No? Well, let me tell you.")
+    end
+  end
+end
